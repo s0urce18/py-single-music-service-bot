5764e56207c81c37a467ef3052c0060110fd7996
s0urce18 <54136752+s0urce18@users.noreply.github.com>
2022-05-26 07:42:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,73 @@
 # imports ----------------------------------
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from Song import Song # Songs.py
+from States import States # States.py
+from BotBase import AdminBotBase, BotBaseCommands # BotBase.py
 import json
 # ------------------------------------------
 
-# bot and dispatcher  creating -------------
-bot_config_file = open("bot_config.json") # opening bot_config.json
+# bot, dispatcher, states and admin base creating -------------
+bot_config_file = open("bot_config.json", encoding="utf-8") # opening bot_config.json
 
 """bot_config.json:
 {
-    "token": *BOT TOKEN*
+    "token": *BOT TOKEN*,
+    "admin": {
+        "admin_password": *ADMIN PASSWORD*,
+        "admin_username": *ADMIN USERNAME*,
+        "admin_id": *ADMIN ID*
+    },
+    "messages": {
+        "start_message": [
+            *START MESSAGE 1*,
+            *START MESSAGE 2*,
+            ...
+            *START MESSAGE N*
+        ],
+        "help_message": [
+            *HELP MESSAGE 1*,
+            *HELP MESSAGE 2*,
+            ...
+            *HELP MESSAGE N*
+        ],
+        "about_message": [
+            *ABOUT MESSAGE 1*,
+            *ABOUT MESSAGE 2*,
+            ...
+            *ABOUT MESSAGE N*
+        ],
+        "test_message": [
+            *TEST MESSAGE 1*,
+            *TEST MESSAGE 2*,
+            ...
+            *TEST MESSAGE N*
+        ]
+    }
 }
 """
 
 bot_config_dict: dict = json.loads(bot_config_file.read()) # extracting from json to dict
 bot_config_file.close() # closing bot_config.json 
 bot: Bot = Bot(token=bot_config_dict["token"]) # creating bot
-dp: Dispatcher = Dispatcher(bot) # creating dispatcher
+dp: Dispatcher = Dispatcher(bot, storage=MemoryStorage()) # creating dispatcher
+states: States = States() # creating states
+admin_bot_base: AdminBotBase = AdminBotBase(bot, dp, states, **bot_config_dict["admin"]) # creating admin base
+bot_base_commands: BotBaseCommands = BotBaseCommands(bot, dp, **bot_config_dict["messages"]) # creating base commands
+# -----------------------------------------
+
+# registering admin messages --------------
+dp.register_message_handler(admin_bot_base.admin_command, commands=["admin"]) # admin command
+dp.register_message_handler(admin_bot_base.admin_message_state_admin, state=states.admin) # working with messages with state admin
+dp.register_message_handler(admin_bot_base.review_command, commands=["review"]) # review command
+dp.register_message_handler(admin_bot_base.review_to_admin, state=states.review) # giving review to admin
+# -----------------------------------------
+
+# registering base commands ---------------
+dp.register_message_handler(bot_base_commands.start_command, commands=["start"]) # start command
+dp.register_message_handler(bot_base_commands.help_command, commands=["help"]) # help command
+dp.register_message_handler(bot_base_commands.about_command, commands=["about"]) # about command
+dp.register_message_handler(bot_base_commands.test_command, commands=["test"]) # test command
 # -----------------------------------------
 
 @dp.message_handler()
@@ -37,5 +88,7 @@ async def main(message: types.Message) -> None: # answer on all messages
         await message.answer("Finding by query...")
         song = Song(name=text)
         query = "Query"
-    # answer:
-    await message.answer(f"{query}: {song.get_name()}\nYT Music: {song.get_ytm_link()}\nSpotify: {song.get_sp_link()}\nDeezer: {song.get_dz_link()}")
+    await message.answer(f"{query}: {song.get_name()}\n" +
+                         f"YT Music: {song.get_ytm_link()}\n" +
+                         f"Spotify: {song.get_sp_link()}\n" +
+                         f"Deezer: {song.get_dz_link()}") # answer
